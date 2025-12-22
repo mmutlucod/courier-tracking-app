5bbff001fe5faf5c83aedd21a7c1895c86fbcc1e
mmutlucod <110405404+mmutlucod@users.noreply.github.com>
2025-12-22 20:39:58 +0300

    images fixed.

--- frontend/babel.config.js
+++ frontend/babel.config.js
@@ -1,10 +1,9 @@
-// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      'react-native-reanimated/plugin', // ✅ En sona
+      'react-native-reanimated/plugin',
     ],
   };
 };
